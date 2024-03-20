3e698a88c22d8bb457eb1788ce4d3c79bc868755
ggusak <ggusak96@gmail.com>
2024-03-20 16:34:07 +0100

    add sequence for convert temp

--- temperature-logger-code/temperature-logger-code.ino
+++ temperature-logger-code/temperature-logger-code.ino
@@ -2,7 +2,7 @@
 
 #include OneWire.h
 #define DS18B20_Pin 8
-Onewire on(DS18B20_Pin)
+Onewire on(DS18B20_Pin);
 
 
 void setup()
@@ -12,7 +12,26 @@ void setup()
 
 void loop()
 {
-  //1st sequence: initialization
-  //2nd sequence: ROM command
-  //3rd sequence: DS18B20 command  
+    /*
+    We always need to follow this sequence
+    1st sequence: initialization
+    2nd sequence: ROM command
+    3rd sequence: DS18B20 command 
+    */
+  byte rom_code[8]:  
+
+  ow.reset();
+  ow.write(0x33)
+  
+  for (int i=0; i<8; i++)
+  {
+    rom_code[i] = ow.read();
+  }
+  
+    //Start sequence to convert temp
+
+  ow.reset();
+  ow.write(0xCC);
+  ow.write(0x44);
+
 }
